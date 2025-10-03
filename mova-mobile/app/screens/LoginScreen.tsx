a9303f4dae29d9e29e6d5cbe63ae50781cde4e8c
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-10-03 19:30:21 +0200

    fix: update import path for CustomTextInput in LoginScreen

--- mova-mobile/app/screens/LoginScreen.tsx
+++ mova-mobile/app/screens/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, Pressable, useWindowDimensions } from 'react-native';
 import GradientBackground from '@/components/ui/ColorBackground';
-import CustomTextInput from '../../components/ui/LoginInput';
+import CustomTextInput from '../../components/ui/TextInput';
 import CustomCard from '../../components/ui/WhiteFrame';
 import MovaLogo from '../../components/ui/MovaLogo';
 import GenericButton from '@/components/ui/GenericButton';
