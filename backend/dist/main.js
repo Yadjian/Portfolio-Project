2f1bda73670cd1e1b45485ea1a716e3eb75e797c
Steph974-Git <10466@holbertonstudents.com>
2025-10-03 11:05:29 +0200

    Refactor code structure for improved readability and maintainability

---
+++ backend/dist/main.js
@@ -0,0 +1,57 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const fs = __importStar(require("fs"));
+const yaml = __importStar(require("js-yaml"));
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    console.log('Database URL:', process.env.DATABASE_URL);
+    console.log('Redis URL:', process.env.REDIS_URL);
+    console.log('JWT Secret:', process.env.JWT_SECRET);
+    console.log('Port:', process.env.PORT);
+    const fileContents = fs.readFileSync('openapi.yml', 'utf8');
+    const document = yaml.load(fileContents);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Swagger UI is available on http://localhost:${port}/api`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
