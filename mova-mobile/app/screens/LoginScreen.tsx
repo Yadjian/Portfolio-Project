4a8c8cd0e7d4da4aff5f6565c1e585f9a45315a9
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-10-03 20:42:46 +0200

    fix: update navigation for account creation link in LoginScreen

--- mova-mobile/app/screens/LoginScreen.tsx
+++ mova-mobile/app/screens/LoginScreen.tsx
@@ -61,13 +61,8 @@ export default function LoginScreen({ navigation }: any) {
               marginBottom: height * 0.01,
             }}
           >
-            <Pressable onPress={() => navigation.navigate('Register')}>
-              <Text
-                style={{
-                  color: '#6746a8',
-                  fontSize: width * 0.033,
-                }}
-              >
+            <Pressable onPress={() => navigation.navigate('ChooseRegisterType')}>
+              <Text style={{ color: '#6746a8', fontSize: width * 0.033 }}>
                 Créer mon compte
               </Text>
             </Pressable>
