322649a89a223b27a3266c96ee43c1276ed58949
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-10-21 19:42:56 +0200

    feat: add dotenv for environment variable management and update .gitignore

---
+++ mova-mobile/app.config.js
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'mova-mobile',
+    slug: 'mova-mobile',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'movamobile',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/images/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: ['expo-router', 'expo-secure-store', 'expo-font'],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      // C'est ici qu'on rend la clé disponible dans l'application
+      googlePlacesApiKey: process.env.GOOGLE_PLACES_API_KEY,
+    },
+  },
+};
