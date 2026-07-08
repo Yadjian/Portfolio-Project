9ad6a0cf870225dc52ad135ae14c4fc477e2ff23
Lucas-Boyadjian <10462@holbertonstudents.com>
2026-07-08 17:45:40 +0200

    refactor: update rate limits for throttling in app module and auth controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -62,12 +62,12 @@ const getBullImports = () => {
       {
         name: 'default',
         ttl: 60000, // 1 minute
-        limit: 80, // 80 requests per minute
+        limit: 200, // 200 requests per minute
       },
       {
         name: 'auth',
         ttl: 900000, // 15 minutes
-        limit: 5, // 5 login attempts per 15 minutes
+        limit: 10, // 10 login attempts per 15 minutes
       },
     ]),
   ],
